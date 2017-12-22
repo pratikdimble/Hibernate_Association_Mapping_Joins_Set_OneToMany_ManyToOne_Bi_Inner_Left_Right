c106cb9669785bc07aaf04c9d394fc2bd515f449
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-22 09:58:15 -0800

    Create PhoneNumber.java

---
+++ src/java/main/com/pratik/domain/PhoneNumber.java
@@ -0,0 +1,50 @@
+package com.pratik.domain;
+
+public class PhoneNumber {
+	private long phone;
+	private String numberType;
+	private String operator;
+	private User person;
+	public PhoneNumber() {
+		
+		System.out.println("PhoneNumber:0-param constructor");
+	}
+	
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getNumberType() {
+		return numberType;
+	}
+	public void setNumberType(String numberType) {
+		this.numberType = numberType;
+	}
+	
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public User getPerson() {
+		return person;
+	}
+
+	public void setPerson(User person) {
+		this.person = person;
+	}
+
+	@Override
+	public String toString() {
+		return "PhoneNumber [phone=" + phone + ", numberType=" + numberType + ", operator=" + operator + ", person="
+				+ person + "]";
+	}
+
+
+
+}

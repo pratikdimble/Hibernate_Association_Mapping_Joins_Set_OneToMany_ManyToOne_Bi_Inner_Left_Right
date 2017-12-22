9ad67e8150c630f6a05d8ab2b8f57f28d8a3aca4
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-22 10:00:18 -0800

    Add files via upload

---
+++ src/java/main/com/pratik/test/OTM_Test_LoadData_RightJoin.java
@@ -0,0 +1,18 @@
+package com.pratik.test;
+
+import com.pratik.dao.OTM_DAO;
+import com.pratik.dao.OTM_DAOFactory;
+import com.pratik.utility.HibernateUtil;
+
+public class OTM_Test_LoadData_RightJoin {
+	public static void main(String[] args) {
+		OTM_DAO dao=null;
+		//get DAO
+		dao=OTM_DAOFactory.getInstance();
+		//save objs
+		dao.loadDataUsingChildRightJoin();
+		//close objs
+		HibernateUtil.closeSession();
+		HibernateUtil.closeSessionFactory();
+	}//main
+}//class
